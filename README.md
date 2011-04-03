ac8f8d84f46f7afac328e2f346240866399ae233
Kevin Henderson <pyspud@gmail.com>
2011-04-03 18:00:06 +0100

    Initial files for Seismic App.
    Files generated by Eclipse for an example Earthquake app from Professional Android 2 Application Development by Reto Meier

---
+++ README.md
@@ -0,0 +1,10 @@
+Seismic
+=======
+
+This project is for learning Android development.
+
+SHOULD NOT BE USED AS WILL KILL PUPPIES.
+
+Application based on the Example Earthquake application from Professional Android 2 Application Development Book by Reto Meier
+
+
---
+++ src/uk/co/kalgan/app/seismic/Seismic.java
@@ -0,0 +1,13 @@
+package uk.co.kalgan.app.seismic;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Seismic extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
